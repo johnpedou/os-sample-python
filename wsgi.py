cd756009ae30fea0dcdb790255aa63a46cd5a00a
root <root@ip-172-31-17-241.us-west-2.compute.internal>
2017-01-16 06:25:17 +0000

    changing message

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello my World!"
+    return "Hello welcome to my World!"
 
 if __name__ == "__main__":
     application.run()
